4e945a13197096edc79d49c3027bf8141c3ac153
Felix Mattick <felix.mattick@gmail.com>
2020-10-31 22:31:54 -0400

    Util to convert game codes to strings

--- packages/util/src/codes.test.ts
+++ packages/util/src/codes.test.ts
@@ -1,8 +1,15 @@
-import { v2CodeToNumber } from './codes'
+import { v2CodeToNumber, v2NumberToCode } from './codes'
 
 test('v2 codes convert to numbers correctly', () => {
   expect(v2CodeToNumber('AAAAAA')).toBe(-1679540573)
   expect(v2CodeToNumber('ABCDEF')).toBe(-1943683525)
   expect(v2CodeToNumber('UVWXYZ')).toBe(-1838004714)
   expect(v2CodeToNumber('EIVKQQ')).toBe(-2147036604)
+})
+
+test('v2 numbers convert to codes correctly', () => {
+  expect(v2NumberToCode(-1679540573)).toBe('AAAAAA')
+  expect(v2NumberToCode(-1943683525)).toBe('ABCDEF')
+  expect(v2NumberToCode(-1838004714)).toBe('UVWXYZ')
+  expect(v2NumberToCode(-2147036604)).toBe('EIVKQQ')
 })
--- packages/util/src/codes.ts
+++ packages/util/src/codes.ts
@@ -1,33 +1,9 @@
 // https://wiki.weewoo.net/wiki/Game_Codes
 
-const v2LookupTable: Record<number, number> = {
-  0: 0x19,
-  1: 0x15,
-  2: 0x13,
-  3: 0x0a,
-  4: 0x08,
-  5: 0x0b,
-  6: 0x0c,
-  7: 0x0d,
-  8: 0x16,
-  9: 0x0f,
-  10: 0x10,
-  11: 0x06,
-  12: 0x18,
-  13: 0x17,
-  14: 0x12,
-  15: 0x07,
-  16: 0x00,
-  17: 0x03,
-  18: 0x09,
-  19: 0x04,
-  20: 0x0e,
-  21: 0x14,
-  22: 0x01,
-  23: 0x02,
-  24: 0x05,
-  25: 0x11
-}
+const v2Characters = 'QWXRTYLPESDFGHUJKZOCVBINMA'
+const v2LookupTable = [
+  25, 21, 19, 10, 8, 11, 12, 13, 22, 15, 16, 6, 24, 23, 18, 7, 0, 3, 9, 4, 14, 20, 1, 2, 5, 17
+]
 
 /**
  * Converts a V2 game code into its number form.
@@ -35,14 +11,35 @@ const v2LookupTable: Record<number, number> = {
  * @param code Game code as a string
  */
 export const v2CodeToNumber = (code: string) => {
-  const b1 = v2LookupTable[code.toUpperCase()[0].charCodeAt(0) - 65]
-  const b2 = v2LookupTable[code.toUpperCase()[1].charCodeAt(0) - 65]
-  const b3 = v2LookupTable[code.toUpperCase()[2].charCodeAt(0) - 65]
-  const b4 = v2LookupTable[code.toUpperCase()[3].charCodeAt(0) - 65]
-  const b5 = v2LookupTable[code.toUpperCase()[4].charCodeAt(0) - 65]
-  const b6 = v2LookupTable[code.toUpperCase()[5].charCodeAt(0) - 65]
+  code = code.toUpperCase()
 
-  const lsb = (b1 + 26 * b2) & 0x3ff
-  const msb = b3 + 26 * (b4 + 26 * (b5 + 26 * b6))
-  return lsb | ((msb << 10) & 0x3ffffc00) | 0x80000000
+  const a = v2LookupTable[code[0].charCodeAt(0) - 65]
+  const b = v2LookupTable[code[1].charCodeAt(0) - 65]
+  const c = v2LookupTable[code[2].charCodeAt(0) - 65]
+  const d = v2LookupTable[code[3].charCodeAt(0) - 65]
+  const e = v2LookupTable[code[4].charCodeAt(0) - 65]
+  const f = v2LookupTable[code[5].charCodeAt(0) - 65]
+
+  const one = (a + 26 * b) & 0x3ff
+  const two = c + 26 * (d + 26 * (e + 26 * f))
+  return one | ((two << 10) & 0x3ffffc00) | 0x80000000
+}
+
+/**
+ * Converts a V2 game code as a number into a human-readable string
+ * 
+ * @param code Game code as a number
+ */
+export const v2NumberToCode = (code: number) => {
+  const a = code & 0x3ff
+  const b = (code >> 10) & 0xfffff
+
+  return [
+    v2Characters[a % 26],
+    v2Characters[Math.trunc(a / 26)],
+    v2Characters[b % 26],
+    v2Characters[Math.trunc(b / 26 % 26)],
+    v2Characters[Math.trunc(b / (26 * 26) % 26)],
+    v2Characters[Math.trunc(b / (26 * 26 * 26) % 26)]
+  ].join('')
 }
